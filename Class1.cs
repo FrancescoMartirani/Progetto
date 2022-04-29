4d184d57a39d68f0fef014951833eaaadada2d97
FrancescoMartirani <104638397+FrancescoMartirani@users.noreply.github.com>
2022-04-29 16:46:00 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHelper
+{
+    public class Prodotto
+    {
+        public int idprodotto{ get; set; }
+        public string nomeprodotto{ get; set; }
+        public int quantità{ get; set; }
+
+    }
+}
